3d48c6fa51d0ad1e4dd096fce91570b1d640f1bc
TeoStorm7 <108464281+TeoStorm7@users.noreply.github.com>
2023-09-07 16:30:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include "hist.cpp"
 
-int main()
-{
+int main() {
+    hist histogram(10, 0.0, 10.0); 
   
+    histogram.setBinContent(3, 8.0);
+
+    uint32_t bin = 3;
+    std::cout << "Bin " << bin << " content: " << histogram.getBinContent(bin) << std::endl;
+  
+    std::cout << "Number of bins: " << histogram.getNBins() << std::endl;
+  
+    std::cout << "Integral: " << histogram.getIntegral() << std::endl;
+
+    histogram.clearHist();
+
+    return 0;
 }
