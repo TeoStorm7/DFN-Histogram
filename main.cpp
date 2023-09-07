f86b8907bdb1c92761bebe87be6fb95672eb4b4f
TeoStorm7 <teodorfurtuna@yahoo.com>
2023-09-07 16:34:51 +0300

    git commit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
+#include <iostream>
 
-int main() {
-    hist histogram(10, 0.0, 10.0); 
-  
+int main()
+{
+    hist histogram(10, 0.0, 10.0);
+
     histogram.setBinContent(3, 8.0);
 
     uint32_t bin = 3;
     std::cout << "Bin " << bin << " content: " << histogram.getBinContent(bin) << std::endl;
-  
+
     std::cout << "Number of bins: " << histogram.getNBins() << std::endl;
-  
+
     std::cout << "Integral: " << histogram.getIntegral() << std::endl;
 
     histogram.clearHist();
