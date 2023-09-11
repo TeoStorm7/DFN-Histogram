4db16811f1add02ffe09255ef4fd054b87aa45b2
TeoStorm7 <teodorfurtuna@yahoo.com>
2023-09-11 12:50:47 +0300

    git commit hist.h git commit main.cpp

--- hist.h
+++ hist.h
@@ -5,54 +5,53 @@
 class hist
 {
 public:
-    hist(uint32_t nBins, double xMin, double xMax) // constructor of the histogram
+    hist(uint32_t nBins, double xMin, double xMax) // constructor of histogram
     {
         this->nBins = nBins;
         this->xMin = xMin;
         this->xMax = xMax;
+        histData = std::vector<double>(nBins, 0.0); // resize of histData to nBins
     };
 
-    uint32_t getNBins() // returns the number of bins
+    uint32_t getNBins() // return the number of bins
     {
         return nBins;
     };
 
     void fill(double x) // fills the histogram
     {
-        for (uint32_t bin = 0; bin < nBins; bin++)
+        if (x >= xMin && x <= xMax)
         {
-            if (x >= xMin && x <= xMax)
+            uint32_t bin = static_cast<uint32_t>((x - xMin) / (xMax - xMin) * nBins);
+
+            if (bin < nBins)
             {
-              
-            // Fill the area between bin and bin+1 as X and bin content as Y
-                double binContent = getBinContent(bin);
-                double fillValue = binContent * (bin + 1 - x);
-                setBinContent(bin, fillValue);
+                histData[bin]++; // Add to any bin existing
             }
         }
     };
 
     void setBinContent(uint32_t bin, double content) // sets the bin content
     {
-        if (bin >= 0 && bin < nBins)
+        if (bin < nBins)
         {
             histData[bin] = content;
         }
     };
 
-    uint32_t getBinContent(uint32_t bin) // returns the bin content
+    double getBinContent(uint32_t bin) // returns the bin content
     {
-        if (bin >= 0 && bin < nBins)
+        if (bin < nBins)
         {
             return histData[bin];
         }
-        return 0;
+        return 0.0;
     }
 
-    uint32_t getIntegral() // returns the integral
+    double getIntegral() // returns the integral, no need of return 0
     {
-        uint32_t integral = 0;
-        for (uint32_t binContent : histData)
+        double integral = 0.0;
+        for (double binContent : histData)
         {
             integral += binContent;
         }
@@ -61,13 +60,10 @@ public:
 
     void clearHist() // clears the histogram
     {
-        for (uint32_t &binContent : histData)
-        {
-            binContent = 0;
-        }
+        histData = std::vector<double>(nBins, 0.0);
     };
 
-    std::vector<uint32_t> getHistData() // returns the histogram data
+    std::vector<double> getHistData() // returns the histogram
     {
         return histData;
     };
@@ -76,5 +72,5 @@ private:
     uint32_t nBins;
     double xMin;
     double xMax;
-    std::vector<uint32_t> histData;
+    std::vector<double> histData;
 };
--- main.cpp
+++ main.cpp
@@ -3,19 +3,51 @@
 
 int main()
 {
-    hist histogram(10, 0.0, 10.0);
+    hist histogram(10, 0.0, 10.0); // constructor of histogram
 
-    histogram.setBinContent(3, 8.0);
+    // Fill the histogram
+    histogram.fill(2.5);
+    histogram.fill(3.5);
+    histogram.fill(3);
+    histogram.fill(9);
+    histogram.fill(5.5);
+    histogram.fill(4);
+    histogram.fill(4.5);
 
-    uint32_t bin = 3;
-
-    std::cout << "Bin " << bin << " content: " << histogram.getBinContent(bin) << std::endl;
-
+    // Test getNBins
     std::cout << "Number of bins: " << histogram.getNBins() << std::endl;
 
-    std::cout << "Integral: " << histogram.getIntegral() << std::endl;
+    // Test getBinContent
+    std::cout << "Bin Contents:" << std::endl;
+    for (uint32_t bin = 0; bin < histogram.getNBins(); bin++)
+    {
+        double binContent = histogram.getBinContent(bin);
+        std::cout << "Bin " << bin << ": " << binContent << std::endl;
+    }
 
+    // Test getIntegral
+    double integral = histogram.getIntegral();
+    std::cout << "Integral: " << integral << std::endl;
+
+    // Test setBinContent
+    histogram.setBinContent(1, 5);  //  in bin 1, set the content to 5, initally it was 0
+    histogram.setBinContent(2, 10); //  in bin 2, set the content to 10, initially it was 1
+
+    for (uint32_t bin = 0; bin < histogram.getNBins(); bin++)
+    {
+        double binContent = histogram.getBinContent(bin);
+        std::cout << "Bin content after setBinContent " << bin << ": " << binContent << std::endl;
+    }
+
+    // Test clearHist
     histogram.clearHist();
+
+    // Test getHistData
+    std::vector<double> histData = histogram.getHistData();
+    for (double binContent : histData)
+    {
+        std::cout << "Bin Content after clearHist: " << binContent << std::endl;
+    }
 
     return 0;
 }
